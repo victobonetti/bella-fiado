dda9b0a35d891c5ac320c1f8646a26e5a10654cf
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-04-13 16:04:52 -0300

    System working

--- bella-fiado/src/App.tsx
+++ bella-fiado/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import AuthService from './AuthService';
 import { Link } from 'react-router-dom';
 import { removeAcentosEMaiusculas } from './AdminPages/stringFunctions';
+import { Loader } from './AdminPages/AdminPages_components/Loader_Error/Loader_Error/Loader';
 
 
 const LOGIN_URL = 'https://bella-fiado-api-victobonetti.vercel.app/users/login/';
@@ -15,10 +16,13 @@ function App() {
   const [getUsername, setUsername] = useState<string>('');
   const [getPassword, setPassword] = useState<string>('');
   const [userInfo, setUserInfo] = useState([]);
+  const [load, setLoad] = useState(false);
 
 
   const sendLoginInfo = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    setLoad(true)
 
     const authService = new AuthService({ apiUrl: LOGIN_URL })
     const username = removeAcentosEMaiusculas(getUsername);
@@ -38,25 +42,32 @@ function App() {
         } else {
           throw new Error('Não foi possível obter nome ou _id');
         }
-      
+
 
       })
-      .catch((error: Error) => console.error(`Erro ao autenticar: ${error.message}`));
+      .catch((error: Error) => {
+        setLoad(false)
+        alert(`Usuário ou senha incorreto!`)})
   }
 
 
-  return (
-    <div className=' h-screen flex flex-col items-center justify-center bg-neutral-800 text-neutral-800'>
-      <h1 className=' select-none text-4xl mb-4 text-gray-100'>Fiado Bella Pizzaria</h1>
-      <form className=' rounded p-4 bg-gradient-to-b from-orange-400 to-orange-500 w-2/3 lg:w-1/4 h-96 flex flex-col ' action="submit" onSubmit={sendLoginInfo}>
-        <label className=' text-2xl' htmlFor="username">Usuário:</label>
-        <input autoComplete='off' onChange={e => setUsername(e.target.value)} placeholder='Digite seu usuário' maxLength={8} className=' border border-orange-600 mb-2 rounded text-3xl px-2 pb-1' type="text" name='username' id='username' />
-        <label className=' text-2xl' htmlFor="password">Senha:</label>
-        <input onChange={e => setPassword(e.target.value)} placeholder='****' className=' border border-orange-600 shadow-inner rounded  text-3xl px-2 pb-1' type="password" name='password' id='password' maxLength={4} />
-        <button autoFocus type='submit' className=' mt-4 py-2 px-6 text-2xl border rounded-lg w-fit self-center border-gray-100 bg-gradient-to-b from-blue-800 to-blue-900 bg-blue-800 text-gray-100 shadow hover:-translate-y-1 hover:shadow-xl transition-all '>Fazer login</button>
-        <Link to={'/admin'} className='text-center mt-8 cursor-pointer text-yellow-200 underline'>Painel do administador</Link>
-      </form>
-    </div>
+  return (<>
+    {load && <Loader />}
+    {
+      !load &&
+      <div className=' h-screen flex flex-col items-center justify-center bg-neutral-800 text-neutral-800'>
+        <h1 className=' select-none text-4xl mb-4 text-gray-100'>Fiado Bella Pizzaria</h1>
+        <form className=' rounded p-4 bg-gradient-to-b from-orange-400 to-orange-500 w-2/3 lg:w-1/4 h-96 flex flex-col ' action="submit" onSubmit={sendLoginInfo}>
+          <label className=' text-2xl' htmlFor="username">Usuário:</label>
+          <input autoComplete='off' onChange={e => setUsername(e.target.value)} placeholder='Digite seu usuário' maxLength={8} className=' border border-orange-600 mb-2 rounded text-3xl px-2 pb-1' type="text" name='username' id='username' />
+          <label className=' text-2xl' htmlFor="password">Senha:</label>
+          <input onChange={e => setPassword(e.target.value)} placeholder='****' className=' border border-orange-600 shadow-inner rounded  text-3xl px-2 pb-1' type="password" name='password' id='password' maxLength={4} />
+          <button autoFocus type='submit' className=' mt-4 py-2 px-6 text-2xl border rounded-lg w-fit self-center border-gray-100 bg-gradient-to-b from-blue-800 to-blue-900 bg-blue-800 text-gray-100 shadow hover:-translate-y-1 hover:shadow-xl transition-all '>Fazer login</button>
+          <Link to={'/admin'} className='text-center mt-8 cursor-pointer text-yellow-200 underline'>Painel do administador</Link>
+        </form>
+      </div>
+    }
+  </>
   )
 }
 
--- bella-fiado/src/UserPages/HelloPage.tsx
+++ bella-fiado/src/UserPages/HelloPage.tsx
@@ -1,23 +1,93 @@
+import { useEffect, useLayoutEffect, useState } from "react";
+import { AccountsServices } from "../AdminPages/AdminServices/AccountsServices";
+import { UserServices } from "../AdminPages/AdminServices/UserServices";
+import { IAccount } from "../Interfaces";
+import { AxiosResponse } from "axios";
+import { capitalize } from "../AdminPages/stringFunctions";
+import { Loader } from "../AdminPages/AdminPages_components/Loader_Error/Loader_Error/Loader";
+
 export function HelloPage() {
+
+    const [accountsData, setAccountsData] = useState<IAccount[]>([]);
+    const [load, setLoad] = useState(false);
+    const [itemsTableActive, setItemsTableActive] = useState(false);
 
     const params = new URLSearchParams(window.location.search);
     const id = params.get('id');
     const name = params.get('username');
 
-    return (
-        <div className=" p-4 h-screen bg-gray-800 text-gray-100 flex justify-center">
-            <div className=" w-full  md:w-1/2">
-                <h1 className=" text-4xl ">Olá, {name}.</h1>
-                <p className=" text-2xl font-thin">Seja bem-vindo de volta à sua conta Fiado.</p>
-                <div className=" rounded-xl p-8 mt-4 flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-300 text-red-500">
-                    <h2 className="  text-6xl">-R$478.00</h2>
-                    <p className=" text-red-500">É o valor em aberto.</p>
+    useEffect(() => {
+
+        if (id) {
+            setLoad(true)
+            AccountsServices.getUserAccounts(id).then((response: AxiosResponse<IAccount[]>) => {
+                setAccountsData(Object.values(response.data));
+            }).catch(() => {
+                alert('Erro ao acessar contas.')
+            }).finally(() => {
+                setLoad(false)
+            })
+        }
+    }, [])
+
+
+    return (<>
+        {!load &&
+            <div className=" p-4 min-h-screen bg-gray-800 text-gray-100 flex justify-center">
+                <div className=" w-full  md:w-1/2">
+                    <h1 className=" text-4xl ">Olá, {name ? capitalize(name) : name}.</h1>
+                    <p className=" text-2xl font-thin">Seja bem-vindo de volta à sua conta Fiado.</p>
+                    
+                        {accountsData?.map((item) => {
+                            return (
+                                <div className=" rounded-xl p-8 mt-4 flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-300 text-neutral-700  ">
+                                    <h1 className=" w-full text-start text-2xl">Conta {item._id}</h1>
+                                    <h2 className=" w-full text-start text-xl">Foram pagos R${item.payments?.reduce((total, payment) => {
+                                        return total + payment.value;
+                                    }, 0).toFixed(2)}</h2>
+                                    <h2 className="w-full text-start text-xl">O valor total da conta é R${item.items?.reduce((total, price) => {
+                                        return total + (price.amount * price.product_id.price)
+                                    }, 0).toFixed(2)}</h2>
+                                    <button onClick={() => setItemsTableActive(!itemsTableActive)} className=" transition-all hover:scale-105 shadow-lg rounded-lg bg-blue-400 text-gray-100 py-2 mt-4 px-4 text-xl self-start">Ver itens...</button>
+
+                                    {itemsTableActive &&
+                                        <table className=" w-full mt-4 table-auto border border-gray-400">
+                                            <thead>
+                                                <tr>
+                                                    <th className="px-4 py-2">Data</th>
+                                                    <th className="px-4 py-2">Produto</th>
+                                                    <th className="px-4 py-2">Preço</th>
+                                                    <th className="px-4 py-2">Quantidade</th>
+                                                    <th className="px-4 py-2">Total</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                                {item.items?.map((item) => {
+                                                    return (
+                                                        <tr className="text-center" key={item.product_id._id}>
+                                                            <td className="border border-gray-400 px-4 py-2">{String(item.date).slice(0,10)}</td>
+                                                            <td className="border border-gray-400 px-4 py-2">{item.product_id.name}</td>
+                                                            <td className="border border-gray-400 px-4 py-2">R${item.product_id.price.toFixed(2)}</td>
+                                                            <td className="border border-gray-400 px-4 py-2">{item.amount}</td>
+                                                            <td className="border border-gray-400 px-4 py-2">R${(item.product_id.price * item.amount).toFixed(2)}</td>
+                                                        </tr>
+                                                    )
+                                                })}
+                                            </tbody>
+                                        </table>
+                                    }
+
+                                </div>
+                            )
+                        })}
+                        {accountsData.length < 1 &&
+                            <h1 className=" text-black text-2xl">Parabéns! Você não tem nada no seu fiado.</h1>
+                        }
+                    </div>
                 </div>
-                <div className=" flex pt-4 justify-between ">
-                    <button className=" rounded-xl text-2xl border px-4 py-2">Ver conta completa</button>
-                    <button className=" rounded-xl text-2xl border px-4 py-2">Lançar novo item</button>
-                </div>
-            </div>
-        </div>
+        }
+        {load && <Loader />}
+    </>
     )
 }
