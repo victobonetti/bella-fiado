5748ad68fbcd25e45a35bf7636e3a0e7fd5a4569
victobonetti <victorbonettixd@gmail.com>
2023-04-14 19:24:29 -0300

    Responsive commit

--- bella-fiado/src/UserPages/HelloPage.tsx
+++ bella-fiado/src/UserPages/HelloPage.tsx
@@ -41,11 +41,11 @@ export function HelloPage() {
                         {accountsData?.map((item) => {
                             return (
                                 <div className=" rounded-xl p-8 mt-4 flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-300 text-neutral-700  ">
-                                    <h1 className=" w-full text-start text-2xl">Conta {item._id}</h1>
-                                    <h2 className=" w-full text-start text-xl">Foram pagos R${item.payments?.reduce((total, payment) => {
+                                    <h1 className=" w-full text-start text-2xl font-bold">Conta</h1>
+                                    <h2 className=" ml-2 w-full text-start">Foram pagos R${item.payments?.reduce((total, payment) => {
                                         return total + payment.value;
                                     }, 0).toFixed(2)}</h2>
-                                    <h2 className="w-full text-start text-xl">O valor total da conta é R${item.items?.reduce((total, price) => {
+                                    <h2 className=" ml-2 w-full text-start ">O valor total da conta é R${item.items?.reduce((total, price) => {
                                         return total + (price.amount * price.product_id.price)
                                     }, 0).toFixed(2)}</h2>
                                     <button onClick={() => setItemsTableActive(!itemsTableActive)} className=" transition-all hover:scale-105 shadow-lg rounded-lg bg-blue-400 text-gray-100 py-2 mt-4 px-4 text-xl self-start">Ver itens...</button>
@@ -54,23 +54,23 @@ export function HelloPage() {
                                         <table className=" w-full mt-4 table-auto border border-gray-400">
                                             <thead>
                                                 <tr>
-                                                    <th className="px-4 py-2">Data</th>
-                                                    <th className="px-4 py-2">Produto</th>
-                                                    <th className="px-4 py-2">Preço</th>
-                                                    <th className="px-4 py-2">Quantidade</th>
-                                                    <th className="px-4 py-2">Total</th>
+                                                    <th className=" text-sm lg:px-4 lg:py-2">Data</th>
+                                                    <th className=" text-sm lg:px-4 lg:py-2">Produto</th>
+                                                    <th className=" text-sm lg:px-4 lg:py-2">Preço</th>
+                                                    <th className=" text-sm lg:px-4 lg:py-2">Quantidade</th>
+                                                    <th className=" text-sm lg:px-4 lg:py-2">Total</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
+                                            <tbody >
 
-                                                {item.items?.map((item) => {
+                                                {item.items?.map((item, index) => {
                                                     return (
-                                                        <tr className="text-center" key={item.product_id._id}>
-                                                            <td className="border border-gray-400 px-4 py-2">{String(item.date).slice(0,10)}</td>
-                                                            <td className="border border-gray-400 px-4 py-2">{item.product_id.name}</td>
-                                                            <td className="border border-gray-400 px-4 py-2">R${item.product_id.price.toFixed(2)}</td>
-                                                            <td className="border border-gray-400 px-4 py-2">{item.amount}</td>
-                                                            <td className="border border-gray-400 px-4 py-2">R${(item.product_id.price * item.amount).toFixed(2)}</td>
+                                                        <tr key={index} className="  text-center">
+                                                            <td className=" text-sm border border-gray-400 lg:px-4 lg:py-2">{String(item.date).slice(0,10)}</td>
+                                                            <td className=" text-sm border border-gray-400 lg:px-4 lg:py-2">{item.product_id.name}</td>
+                                                            <td className=" text-sm border border-gray-400 lg:px-4 lg:py-2">R${item.product_id.price.toFixed(2)}</td>
+                                                            <td className=" text-sm border border-gray-400 lg:px-4 lg:py-2">{item.amount}</td>
+                                                            <td className=" text-sm border border-gray-400 lg:px-4 lg:py-2">R${(item.product_id.price * item.amount).toFixed(2)}</td>
                                                         </tr>
                                                     )
                                                 })}
